c3a15124c012719c4f0e60faa3325f1866fe06ae
GhostClock <zhangyuanlaifen@gmail.com>
2017-09-16 01:24:18 +0800

    添加请求头和cookie

--- Spider/settings.py
+++ Spider/settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import os, sys
 # Scrapy settings for Spider project
 #
 # For simplicity, this file contains only settings considered important or
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'Spider.spiders'
 #USER_AGENT = 'Spider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False  #  不让遵循robots协议
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -33,16 +33,21 @@ ROBOTSTXT_OBEY = True
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
+COOKIES_DEBUG = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Language': 'en',
+  'Host': 'www.lagou.com',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36',
+  'Referer': 'https://www.lagou.com/',
+  'Cookie': "user_trace_token=20170910195047-4916b413-961e-11e7-8dfd-525400f775ce; LGUID=20170910195047-4916b671-961e-11e7-8dfd-525400f775ce; TG-TRACK-CODE=index_hotjob; X_HTTP_TOKEN=e36e4321d3a1b43f5c552ede6ab0a7cf; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1505055149; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1505406022; JSESSIONID=ABAAABAAAFCAAEG49D7D825F902299D603EA696BC9AC967; _putrc=67B31373EFD0463B; login=true; unick=%E5%BC%A0%E5%BF%97%E8%BF%9C; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=56; SEARCH_ID=22778d5724ac4ba88f6c9b4a253199c2; index_location_city=%E6%B7%B1%E5%9C%B3; _ga=GA1.2.512492824.1505044247; LGSID=20170915235604-60f4d238-9a2e-11e7-9196-5254005c3644; LGRID=20170916000834-202e46ee-9a30-11e7-9196-5254005c3644"
+}
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
@@ -68,7 +73,6 @@ ROBOTSTXT_OBEY = True
 #    'Spider.pipelines.SpiderPipeline': 300,
 #}
 
-import os, sys
 BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 sys.path.insert(0, os.path.join(BASE_DIR, "Spider"))    # 把路径加入到Python path中
 
