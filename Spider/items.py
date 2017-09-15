b16489f5adfcbf181832479f44670236b3f87bfc
GhostClock <zhangyuanlaifen@gmail.com>
2017-09-16 01:21:51 +0800

    item中拉勾网职位信息

--- Spider/items.py
+++ Spider/items.py
@@ -12,3 +12,44 @@ class SpiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class LagouJob(scrapy.Item):
+    #   拉勾网职位信息
+    title = scrapy.Field()
+
+    url = scrapy.Field()
+
+    uil_object_id = scrapy.Field()
+
+    salary_min = scrapy.Field()
+
+    salary_max = scrapy.Field()
+
+    job_city = scrapy.Field()
+
+    work_years_min = scrapy.Field()
+
+    work_years_max = scrapy.Field()
+
+    degree_need = scrapy.Field()
+
+    job_type = scrapy.Field()
+
+    pulish_time = scrapy.Field()
+
+    tags = scrapy.Field()
+
+    job_advantage = scrapy.Field()
+
+    job_desc = scrapy.Field()
+
+    job_addr = scrapy.Field()
+
+    company_url = scrapy.Field()
+
+    company_name = scrapy.Field()
+
+    crawl_time = scrapy.Field()
+
+    crawl_update_time = scrapy.Field()
