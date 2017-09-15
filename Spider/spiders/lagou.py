e2eb155971d5bb1f038e6399f1343b8a52a8451d
GhostClock <zhangyuanlaifen@gmail.com>
2017-09-16 01:28:22 +0800

    添加需要爬取的规则

--- Spider/spiders/lagou.py
+++ Spider/spiders/lagou.py
@@ -1,19 +1,41 @@
 # -*- coding: utf-8 -*-
-import scrapy
+
+import os
+import json
+import re
+import requests
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-
+from scrapy.http import Request, FormRequest
+from time import time
+from PIL import Image
+from hashlib import md5
+from Spider.settings import DEFAULT_REQUEST_HEADERS
+try:
+    import cookielib
+except:
+    import http.cookiejar as cookielib
 
 class LagouSpider(CrawlSpider):
     name = 'lagou'
     allowed_domains = ['www.lagou.com']
-    start_urls = ['http://www.lagou.com/']
+    start_urls = ['https://www.lagou.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=('zhaopin/.*', )), follow=True),
+        Rule(LinkExtractor(allow=('gongsi/j\d+.html', )), follow=True),
+        # 如果连接满足下面这种形式，就会调到callback里面的方法去支持 allow里面传递的是正则表达式
+        Rule(LinkExtractor(allow=r'jobs/\d+.html'), callback='parse_job', follow=True),
     )
 
-    def parse_item(self, response):
+    def parse_start_url(self, response):
+        return []
+
+    def process_results(self, response, results):
+        return results
+
+    def parse_job(self, response):
+        #   解析拉勾网的职位
         i = {}
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
