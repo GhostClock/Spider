3bee2e946b5022002fcfb0496e50298e309dcbba
GhostClock <zhangyuanlaifen@gmail.com>
2017-09-29 23:18:53 +0800

    爬取知乎命令

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))   # 设置工程目录
 
-execute(["scrapy", "crawl", "lagou"])
+execute(["scrapy", "crawl", "zhihu"])   # 知乎
+
+# execute(["scrapy", "crawl", "lagou"])   # 拉勾
