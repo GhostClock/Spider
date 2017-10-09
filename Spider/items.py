054d611262a91519b470a70c5722d1a1fd8ff40a
GhostClock <zhangyuanlaifen@gmail.com>
2017-10-09 22:52:41 +0800

    把数据插入到数据库

--- Spider/items.py
+++ Spider/items.py
@@ -12,6 +12,7 @@ from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst, MapCompose, Join
 from w3lib.html import remove_tags
 from Spider.settings import SQL_DATETIME_FORMAT, SQL_DATE_FORMAT
+from Spider.utils.common import extract_num
 
 class SpiderItem(scrapy.Item):
     # define the fields for your item here like:
@@ -119,5 +120,133 @@ class LagouJobItem(scrapy.Item):
             self['pulish_time'], self['tags'], self['job_advantage'],
             self['job_desc'], self['job_addr'], self['company_url'],
             self['company_name'], self['crawl_time'].strftime(SQL_DATETIME_FORMAT))
+
+        return insert_sql, params
+
+
+
+class ZhihuQuestionItem(scrapy.Item):
+    """
+    知乎的问题Item
+    """
+    zhihu_id = scrapy.Field()
+
+    topics = scrapy.Field()
+
+    url = scrapy.Field()
+
+    title = scrapy.Field()
+
+    content = scrapy.Field()
+
+    answer_num = scrapy.Field()
+
+    comments_num = scrapy.Field()
+
+    watch_num = scrapy.Field()
+
+    click_num = scrapy.Field()
+
+    crawl_num = scrapy.Field()
+
+    def get_insert_sql(self):
+        # 插入知乎question表的sql语句
+        insert_sql = """
+            insert into zhihu_question (
+              zhihu_id, topics, url, title,
+              content, answer_num, comments_num, 
+              watch_user_num, click_num, crawl_time
+            )VALUES (
+              %s, %s, %s, %s, %s,
+              %s, %s, %s, %s, %s
+            )
+            ON DUPLICATE KEY UPDATE
+             topics=VALUES (topics), 
+             title=VALUES (title),
+             content=VALUES (content),
+             answer_num=VALUES (answer_num),
+             comments_num=VALUES (comments_num),
+             watch_user_num=VALUES (watch_user_num),
+             click_num=VALUES (click_num),
+             crawl_time=VALUES (crawl_time)
+        """
+        zhihu_id = self["zhihu_id"][0]
+        topics = ",".join(self["topics"])
+        url = self["url"][0]
+        title = "".join(self["title"])
+        content = "".join(self["content"])
+        answer_num = extract_num("".join(self["answer_num"]))
+        comments_num = extract_num("".join(self["comments_num"]))
+
+        if len(self["watch_num"]) == 2:
+            watch_user_num = int(self["watch_num"][0])
+            click_num = int(self["watch_num"][1])
+        else:
+            watch_user_num = int(self["watch_num"][0])
+            click_num = 0
+
+        crawl_time = datetime.datetime.now().strftime(SQL_DATETIME_FORMAT)
+
+        params = (
+            zhihu_id, topics, url, title,
+            content, answer_num, comments_num,
+            watch_user_num, click_num, crawl_time
+        )
+
+        return insert_sql, params
+
+
+class ZhihuAnswerItem(scrapy.Item):
+    """
+    知乎的问题回答Item
+    """
+    zhihu_id = scrapy.Field()
+
+    url = scrapy.Field()
+
+    question_id = scrapy.Field()
+
+    author_id = scrapy.Field()
+
+    content = scrapy.Field()
+
+    parise_num = scrapy.Field()
+
+    comments_num = scrapy.Field()
+
+    create_time = scrapy.Field()
+
+    update_time = scrapy.Field()
+
+    crawl_time = scrapy.Field()
+
+    def get_insert_sql(self):
+        # 插入知乎question表的sql语句
+        insert_sql = """
+            insert into zhihu_answer (
+              zhihu_id, url, question_id, 
+              author_id, content, comments_num, 
+              praise_num, create_time, 
+              update_time, crawl_time
+            )
+             VALUES (
+              %s, %s, %s, %s, %s,
+              %s, %s, %s, %s, %s
+            )
+            ON DUPLICATE KEY UPDATE
+             content=VALUES (content), 
+             comments_num=VALUES (comments_num),
+             praise_num=VALUES (praise_num),
+             update_time=VALUES (update_time)
+        """
+        create_time = datetime.datetime.fromtimestamp(self["create_time"]).strftime(SQL_DATETIME_FORMAT)
+        update_time = datetime.datetime.fromtimestamp(self["update_time"]).strftime(SQL_DATETIME_FORMAT)
+        params = (
+            self["zhihu_id"], self["url"],
+            self["question_id"], self["author_id"],
+            self["content"], self["comments_num"],
+            self["parise_num"], create_time, update_time,
+            self["crawl_time"].strftime(SQL_DATETIME_FORMAT)
+        )
 
         return insert_sql, params
