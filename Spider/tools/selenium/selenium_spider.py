31bd5e3834499538e6709f3366a1234a485e43f8
GhostClock <zhangyuanlaifen@gmail.com>
2017-10-25 22:15:04 +0800

    测试selenium

---
+++ Spider/tools/selenium/selenium_spider.py
@@ -0,0 +1,11 @@
+# -*- coding=utf-8 -*-
+__author__ = 'ghostclock'
+from selenium import webdriver
+
+# from pyvirtualdisplay import Display
+# display = Display(visible=0, size=(800, 600))
+# display.start()
+browser = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+
+pass
