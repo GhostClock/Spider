a916be55afece2bd4d67cac84612bc474b59a48d
GhostClock <zhangyuanlaifen@gmail.com>
2017-10-26 23:06:59 +0800

    用selenium模拟知乎登陆

--- Spider/tools/selenium/selenium_spider.py
+++ Spider/tools/selenium/selenium_spider.py
@@ -1,11 +1,16 @@
 # -*- coding=utf-8 -*-
 __author__ = 'ghostclock'
+"""
+用slenium来模拟知乎
+"""
 from selenium import webdriver
+from scrapy.selector import Selector
 
-# from pyvirtualdisplay import Display
-# display = Display(visible=0, size=(800, 600))
-# display.start()
-browser = webdriver.Chrome('/usr/local/bin/chromedriver')
+browser = webdriver.Chrome(executable_path='chromedriver.exe')
+browser.get('https://www.zhihu.com/#signin')
 
-
+account = browser.find_element_by_css_selector('.view.view-signin input[name="account"]').send_keys('youAccount')
+browser.find_element_by_css_selector('.view-signin input[name="password"]').send_keys('youPassword')
+browser.find_element_by_css_selector('view-signin button.sign-button')
+browser.quit()
 pass
